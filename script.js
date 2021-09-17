8ab5fabea7c147e3c9342483253c4d7bd33d8f05
Your Name <renee.petit@gmail.com>
2021-09-17 16:12:36 -0700

    currency conversion works

--- script.js
+++ script.js
@@ -13,7 +13,11 @@ function calculate() {
     fetch(`https://v6.exchangerate-api.com/v6/${api_key}/latest/${currency_one}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            //console.log(data);
+            const rate = data.conversion_rates[currency_two];
+            //console.log(rate);
+            rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+            amountEl_two.value = (amountEl_one.value * rate.toFixed(2));
         });
 }
 
@@ -22,6 +26,11 @@ currencyEl_one.addEventListener('change', calculate);
 amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
 amountEl_two.addEventListener('input', calculate);
-
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+})
 
 calculate();
